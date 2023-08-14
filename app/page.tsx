6359cf0e3d10bee5ef3b4faffda72c4d7771cb29
Anzhy11 <harnzzy@gmail.com>
2023-08-14 11:49:17 +0100

    added header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import Board from "@/components/Board";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <main className="flex">
-      <p>Home</p>
+    <main>
+      {/* header */}
+      <Header />
+
+      {/* board  */}
+      <Board />
     </main>
   );
 }
